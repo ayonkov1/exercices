33c31840d0c6c6a1f3d5ee112cc934e03f53a43f
Atanas Yonkov <a.yonkov@student.fontys.nl>
2022-11-17 16:03:16 +0100

    Simple use of API in python
    Calling API dynamically in python
    
    Working with the response

---
+++ python/08_parse-json1.py
@@ -0,0 +1,16 @@
+import requests 
+import urllib.parse 
+
+main_api = "http://www.mapquestapi.com/directions/v2/route?"
+orig = input("Enter current location: ")
+dest = input("Enter destination location: ")
+key = "oxfrVevsHw2iePslAFdMTABdsWjPd0bI"
+
+
+url = main_api + urllib.parse.urlencode({ "key": key, "from": orig, "to": dest})
+
+res = requests.get(url).json()
+
+statuscode = res["info"]["statuscode"]
+
+print(url)
