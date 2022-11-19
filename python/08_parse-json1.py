2b749e680c64179172b816d49e2d4696c22f7c72
ayonkov1 <atanasyonkov1@gmail.com>
2022-11-19 10:30:30 +0100

    Configure format settings for linux

--- python/08_parse-json1.py
+++ python/08_parse-json1.py
@@ -1,13 +1,12 @@
-import requests 
-import urllib.parse 
+import requests
+import urllib.parse
 
 main_api = "http://www.mapquestapi.com/directions/v2/route?"
 orig = input("Enter current location: ")
 dest = input("Enter destination location: ")
 key = "oxfrVevsHw2iePslAFdMTABdsWjPd0bI"
 
-
-url = main_api + urllib.parse.urlencode({ "key": key, "from": orig, "to": dest})
+url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
 
 res = requests.get(url).json()
 
