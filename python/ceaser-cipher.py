83ec1a45bcc2211576aef3ebe3ebc842eee31f74
Atanas Yonkov <a.yonkov@student.fontys.nl>
2022-11-30 22:30:54 +0100

    start ceaser cypher hw

--- python/ceaser-cipher.py
+++ python/ceaser-cipher.py
@@ -1,13 +1,31 @@
 # message = input("Message to be encrypted: ")
 # offset = input("Encryption offset: ")
 
+# def encrypt(msg, n):
+#     print("before", msg)
+#     result = []
+#     encrypted = ""
+#     for i in range(len(msg)):
+#         char = ord(msg[i]) - n + 65
+#         result.append(char)
+#     for e in result:
+#         encrypted += chr(e % 26 + 65)
+#     return encrypted
 
-def encrypt(msg):
-    result = ""
-    for i in range(len(msg)):
-        char = msg[i]  # strings of A S D
-        result += char  # result = A + S + D as string
-    return result
+
+def encrypt(msg, n):
+    print("before", msg)
+    result = []
+    encrypted = ""
+    words = msg.split()
+    for word in words:
+        for letter in word:
+            char = ord(letter)
+            offsetChar = char - n + 65
+            result.append(offsetChar)
+    for number in result:
+        encrypted += chr(number % 26 + 65)
+    return encrypted
 
 
-print(encrypt("ASD"))
+print("after", encrypt("ABC DEF GHI JKL MNO PQR STU VWX YZ", 3))
