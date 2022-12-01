ed1744e7fe5436cc75bfbc8c5055a3ba9ad562ec
Atanas Yonkov <a.yonkov@student.fontys.nl>
2022-12-01 11:16:39 +0100

    Utilize netimiko to connect to Cisco IOS XE

--- python/cisco-ssh.py
+++ python/cisco-ssh.py
@@ -1,7 +1,14 @@
+from netmiko import ConnectHandler
+
+config_commands = ["int loopback 1", "ip address 2.2.2.2 255.255.255.0", "description WHATEVER"]
+
 sshCli = ConnectHandler(
-    device_type: "cisco_ios",
-    host: "10.10.20.48", 
-    port: 22, 
-    username: 'cisco', 
-    port: 'cisco123!', 
+    device_type="cisco_ios",
+    host="10.10.20.48",
+    port=22,
+    username="developer",
+    password="C1sco12345",
 )
+
+output = sshCli.send_config_set(config_commands)
+print(output)
